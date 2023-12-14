16f781601ba1f682db137487d530144446cf60de
mrbayus <mrbayus2014@gmail.com>
2023-12-14 15:49:30 +0100

    Update addition.py
    Print Hello Human

--- src/addition.py
+++ src/addition.py
@@ -1,9 +1,12 @@
 # app.py
 # This is a test commit
-# This is a test commit v2.0
+
 def add(a, b):
     return a + b
 
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
+# This is a test commit v2.0
+print("Hello Human")
